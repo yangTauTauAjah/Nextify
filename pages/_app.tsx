6ec10ab9a1ce9504b499645006c2c2bd7dc83987
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-20 16:59:13 +0700

    adding store to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import '@/styles/globals.css'
 import { themeSettings } from '@/theme'
 import { ThemeProvider, createTheme } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { store } from '@/components/store'
+import { Provider } from 'react-redux'
 
 const Theme = createTheme(themeSettings)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={Theme}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </ThemeProvider>
   )
 }
