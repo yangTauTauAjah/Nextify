c64519c3dccfc4d212ae1863cda247d07b2ed6c3
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-20 16:58:57 +0700

    adding align items

--- components/Layout/NowPlayingBar/VolumeControl.tsx
+++ components/Layout/NowPlayingBar/VolumeControl.tsx
@@ -10,7 +10,7 @@ import CustomSlider from '@/components/CustomSlider';
 
 function VolumeControl() {
   return (
-    <Stack width='33.33%' justifyContent='flex-end' direction='row' gap='1rem'>
+    <Stack width='33.33%' alignItems='center' justifyContent='flex-end' direction='row' gap='1rem'>
       <QueueMusic />
       <DevicesOther />
       <Stack width='50%' direction='row' gap='0.5rem' alignItems='center'>
