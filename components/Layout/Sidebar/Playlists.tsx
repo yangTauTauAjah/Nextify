736d99757d3de3f36aa68c4838f8056bc4dd17f6
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-20 09:46:25 +0700

    create Playlist

---
+++ components/Layout/Sidebar/Playlists.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+import Link from "next/link";
+
+export default function PlayLists(props: { list: Array<{ href: string, text: string }> }) {
+  return (
+    <Box sx={{
+      fontSize: '1rem',
+      overflow: 'auto',
+      marginBlockEnd: 'auto',
+      '&::-webkit-scrollbar': {
+        width: '10px',
+        background: 'rgba(0,0,0,0)',
+      },
+      '&::-webkit-scrollbar-thumb': {
+        background: 'rgba(255,255,255,.0)',
+      },
+      '&:hover::-webkit-scrollbar-thumb': {
+        background: 'rgba(255,255,255,.3)',
+      }
+    }} >{
+        props.list.map((e, i) => (
+          <Link key={i} href={e.href}>
+            <div style={{ height: '2rem', lineHeight: '1.6' }}>{e.text}</div>
+          </Link>
+        ))
+      }</Box>
+  )
+}
