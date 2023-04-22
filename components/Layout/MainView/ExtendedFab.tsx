d1631731a59bd2f3763ae61ae19b16449e1fcf4c
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-22 14:19:27 +0700

    separating style

--- components/Layout/MainView/ExtendedFab.tsx
+++ components/Layout/MainView/ExtendedFab.tsx
@@ -1,18 +1,16 @@
 import { Fab } from '@mui/material'
 import React from 'react'
 
-const ExtendedFab = (props: { children?: string }) => (
-  <Fab
-    sx={{
-      background: 'rgba(255,255,255,0.1)',
-      paddingInline: '1rem',
-      textTransform: 'unset',
-      color: 'white',
-      '&:hover': { background: 'rgba(255,255,255,0.2)' }
-    }}
-    size="small" variant="extended">
-      {props.children}
-  </Fab>
+const FabStyle = {
+  background: 'rgba(255,255,255,0.1)',
+  paddingInline: '1rem',
+  textTransform: 'unset',
+  color: 'white',
+  '&:hover': { background: 'rgba(255,255,255,0.2)' }
+}
+
+const ExtendedFab = ({ children, style }: { children: string, style: any }) => (
+  <Fab sx={{...FabStyle, ...style}} size="small" variant="extended">{children}</Fab>
 )
 
 export default ExtendedFab
