a9110f72cb19730d18bd9077d8cfc8dbb5eb99f5
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-24 21:05:08 +0700

    modify greeting

--- components/Layout/MainView/Recent/Greeting.tsx
+++ components/Layout/MainView/Recent/Greeting.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function Greeting() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <h1>Good morning</h1>
+    <div style={{ marginBlock: '1rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <h1>Selamat beristirahat</h1>
       <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
         <NotificationsNone sx={{ fontSize: '2rem' }} />
         <History sx={{ fontSize: '2rem' }} />
