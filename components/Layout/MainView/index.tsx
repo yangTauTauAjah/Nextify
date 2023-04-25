0b308548eb18ebe3fd2bfc41aa2aa7e9ef33e711
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-25 08:02:07 +0700

    temporary data for testing purpose

--- components/Layout/MainView/index.tsx
+++ components/Layout/MainView/index.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 import Recent from './Recent'
 import Section from './Section'
 import Item from './Section/Collection/Item'
+import { Typography } from '@mui/material/styles/createTypography'
 import TopList from '@/data/categories/toplist.json'
 import Discover from '@/data/featured_6.json'
-import { Typography } from '@mui/material/styles/createTypography'
+import Country from '@/data/categories/Country.json'
+import Indie from '@/data/categories/Indie.json'
+import Gaming from '@/data/categories/Gaming.json'
+import Mood from '@/data/categories/Mood.json'
+import Pop from '@/data/categories/Pop.json'
+import Artist from '@/data/recommendations/artists.json'
 
-const PStyle = ({typography}: {typography: Typography}) => ({
+const PStyle = ({ typography }: { typography: Typography }) => ({
   fontSize: typography.subtitle1.fontSize,
   fontWeight: typography.fontWeightRegular,
   color: typography.subtitle1.color,
@@ -17,58 +23,39 @@ const PStyle = ({typography}: {typography: Typography}) => ({
   overflow: 'hidden'
 })
 
+const Collection = ({ title, type, collection }: { title: string, type?: "playlist" | "artist", collection: any[] } ) => (
+  <Section title={title}>
+    {
+      collection.map(({ id, name, owner, description, images }) => (
+        <Item
+          key={id}
+          image={images[0].url}
+          title={name}
+          href={`/playlist/${id}`}
+          type={type || "playlist"}
+          description={
+            <Box component='p' sx={PStyle}>
+              {description || type === 'playlist' ? `By ${owner?.id}` : 'Artist'}
+            </Box>
+          }
+        />
+      ))
+    }
+  </Section>
+)
+
 function MainView() {
   return (
     <Stack sx={{ gap: '1.5rem', padding: '1rem', marginBottom: '10rem' }}>
       <Recent />
-      <Section title="Top List">
-        {
-          TopList.playlists.items.map(({id, name, description, images }) => (
-            <Item
-              key={id}
-              image={images[0].url}
-              title={name}
-              href={`/playlist/id`}
-              description={
-                <Box component='p' sx={PStyle}>
-                  {description}
-                </Box>
-              }
-            />
-          ))
-        }
-      </Section>
-      <Section title="Discover">
-        {
-          Discover.playlists.items.map(({id, name, description, images }) => (
-            <Item
-              key={id}
-              image={images[0].url}
-              title={name}
-              href={`/playlist/${id}`}
-              description={
-                <Box component='p' sx={PStyle}>
-                  {description}
-                </Box>
-              }
-            />
-          ))
-        }
-      </Section>
-      <Section title="Focus">
-        {
-          [0, 1, 2].map((e, i) => (
-            <Item key={i} image='' title='test' href='' description={<p>Test, test, test</p>} />
-          ))
-        }
-      </Section>
-      <Section title="Spotify Playlists">
-        {
-          [0, 1, 2].map((e, i) => (
-            <Item key={i} image='' title='test' href='' description={<p>Test, test, test</p>} />
-          ))
-        }
-      </Section>
+      <Collection title="Top List" collection={TopList.playlists.items} />
+      <Collection title="Discover" collection={Discover.playlists.items} />
+      <Collection title="Country" collection={Country.playlists.items} />
+      <Collection title="Indie" collection={Indie.playlists.items} />
+      <Collection title="Gaming" collection={Gaming.playlists.items} />
+      <Collection title="Mood" collection={Mood.playlists.items} />
+      <Collection title="Pop" collection={Pop.playlists.items} />
+      <Collection title="Artist" type='artist' collection={Artist.artists} />
     </Stack>
   )
 }
