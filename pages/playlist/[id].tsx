ff59f93690a17f6f04a4ca1943640d3ba59965aa
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-28 06:45:09 +0700

    route for playlist

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,23 +1,15 @@
 import { getPlaylist } from "@/components/stateSlice/SpotifyAPI";
-import {
-  PlaylistObject,
-  UserObject
-} from "@/components/stateSlice/SpotifyAPI/interfaces";
+import { PlaylistObject } from "@/components/stateSlice/SpotifyAPI/interfaces";
 import { GetServerSideProps } from "next";
-import {
-  FavoriteBorderRounded,
-  DownloadRounded,
-  MoreVert,
-  Shuffle,
-  PlayCircleRounded,
-  AccountCircle
-} from "@mui/icons-material";
-import Image from "next/image";
-import Song from "@/components/Playlist/Song";
-import Link from "next/link";
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { GET_PLAYLIST } from "@/components/stateSlice/SpotifyAPI/fields";
-import FilterComponent from "@/components/Playlist/FilterComponent";
+import FilterComponent from "@/components/Layout/MainView/CollectionDisplay/FilterComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { setPlaylist } from "@/components/stateSlice/SpotifyAPI/data";
+import PlaylistThumbnail from "@/components/Layout/MainView/CollectionDisplay/CollectionThumbnail";
+import PlaylistMetadata from "@/components/Layout/MainView/CollectionDisplay/CollectionMetadata";
+import Tracks from "@/components/Layout/MainView/CollectionDisplay/CollectionTracks";
 
 export const getServerSideProps: GetServerSideProps<
   PlaylistObject | {}
@@ -28,107 +20,18 @@ export const getServerSideProps: GetServerSideProps<
   } else return { notFound: true };
 };
 
-const ActionButtonsComponent = () => (
-  <div style={{ display: "flex", gap: "1rem" }}>
-    <FavoriteBorderRounded fontSize="large" />
-    <DownloadRounded fontSize="large" />
-    <MoreVert sx={{ marginRight: "auto" }} fontSize="large" />
-    <Shuffle fontSize="large" />
-    <PlayCircleRounded fontSize="large" />
-  </div>
-);
-
-const PlaylistOwnerComponent = ({ owner }: { owner: UserObject }) => (
-  <div
-    style={{
-      display: "flex",
-      gap: "0.5rem",
-      height: "1.5rem",
-      alignItems: "center"
-    }}>
-    <div
-      style={{
-        height: "100%",
-        aspectRatio: "1",
-        borderRadius: "100px",
-        overflow: "hidden"
-      }}>
-      {owner.images.length > 0 ? (
-        <Image
-          src={owner.images[0].url}
-          fill
-          style={{ objectFit: "contain" }}
-          alt="image"
-        />
-      ) : (
-        <AccountCircle fontSize="large" />
-      )}
-    </div>
-    <Box
-      component={Link}
-      sx={{
-        fontSize: "0.8rem",
-        fontWeight: ({ typography }) => typography.fontWeightBold
-      }}
-      href={`/user/${owner.id}`}>
-      {owner.display_name}
-    </Box>
-  </div>
-);
-
 export default function Id(PlaylistObject: PlaylistObject) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setPlaylist(PlaylistObject));
+  }, [PlaylistObject, dispatch]);
+
   return (
-    <Stack gap={3} sx={{ padding: "1rem" }}>
+    <Stack gap={3} sx={{ padding: "2rem 1rem" }}>
       <FilterComponent />
-      <div style={{ height: "15rem", width: "100%" }}>
-        <Image
-          src={PlaylistObject.images[0].url}
-          fill
-          style={{ objectFit: "contain" }}
-          alt="image"
-        />
-      </div>
-      <Stack
-        gap={1}
-        sx={({ palette, typography }) => ({
-          "& .description, & p": {
-            color: typography.subtitle1.color,
-            "& a": {
-              color: palette.primary.main,
-              borderColor: palette.primary.main,
-              borderBottom: "1px solid"
-            }
-          }
-        })}>
-        <h2>{PlaylistObject.name}</h2>
-        <Box
-          dangerouslySetInnerHTML={{ __html: PlaylistObject.description }}
-          sx={({ typography, palette }) => ({
-            color: typography.subtitle1.color,
-            fontSize: "0.8rem",
-            "& a": {
-              color: palette.primary.main,
-              borderColor: palette.primary.main,
-              borderBottom: "1px solid"
-            }
-          })}
-        />
-        <PlaylistOwnerComponent owner={PlaylistObject.owner} />
-        <Box
-          component={"p"}
-          sx={({ typography }) => ({
-            color: typography.subtitle1.color,
-            fontSize: typography.subtitle1.fontSize
-          })}>
-          24h 60min
-        </Box>
-        <ActionButtonsComponent />
-      </Stack>
-      <Stack gap={2}>
-        {PlaylistObject.tracks.map(({ track }) => (
-          <Song key={track.id} track={track} />
-        ))}
-      </Stack>
+      <PlaylistThumbnail/>
+      <PlaylistMetadata/>
+      <Tracks/>
     </Stack>
   );
 }
