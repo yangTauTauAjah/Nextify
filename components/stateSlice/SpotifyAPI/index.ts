418ebf513d553790aa53454d7745f547b074afbb
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-28 06:44:43 +0700

    add new method

--- components/stateSlice/SpotifyAPI/index.ts
+++ components/stateSlice/SpotifyAPI/index.ts
@@ -1,5 +1,11 @@
 import { GET_PLAYLIST } from "./fields";
-import { BearerToken, PlaylistObject, UserObject } from "./interfaces";
+import {
+  AlbumObject,
+  ArtistObject,
+  BearerToken,
+  PlaylistObject,
+  UserObject
+} from "./interfaces";
 
 const BASE_PATH = "https://api.spotify.com/v1";
 
@@ -17,6 +23,24 @@ async function getAccessToken(): Promise<BearerToken> {
   return data;
 }
 
+export async function getUser(id: string): Promise<UserObject> {
+  const { access_token } = await getAccessToken();
+  let path = `${BASE_PATH}/users/${id}`;
+  const data = await fetch(path, {
+    headers: { Authorization: `Bearer ${access_token}` }
+  }).then((data) => data.json());
+  return data;
+}
+
+export async function getArtist(ids: string[]): Promise<ArtistObject[]> {
+  const { access_token } = await getAccessToken();
+  let path = `${BASE_PATH}/artists?ids=${ids.join(',')}`;
+  const data = await fetch(path, {
+    headers: { Authorization: `Bearer ${access_token}` }
+  }).then((data) => data.json());
+  return data.artists;
+}
+
 export async function getPlaylist(
   id: string,
   fields?: string
@@ -28,20 +52,24 @@ export async function getPlaylist(
     headers: { Authorization: `Bearer ${access_token}` }
   }).then((data) => data.json());
   const owner = await getUser(data.owner.id);
-  return { ...data, owner, tracks: data.tracks.items };
+  return { ...data, owner };
 }
 
-export async function getUser(
-  id: string,
-  fields?: string
-): Promise<UserObject> {
+export async function getAlbum(id: string): Promise<AlbumObject> {
   const { access_token } = await getAccessToken();
-  let path = `${BASE_PATH}/users/${id}`;
-  if (fields && fields !== "") path += `?fields=${fields}`;
-  const data = await fetch(path, {
+  let path = `${BASE_PATH}/albums/${id}`;
+  const data: AlbumObject = await fetch(path, {
     headers: { Authorization: `Bearer ${access_token}` }
   }).then((data) => data.json());
-  return data;
+
+  /* const artists: ArtistObject[] = []
+
+  for (let i = 0; i < data.artists.length; i++) {
+    artists.push(await getArtist(data.artists[i].id))
+  } */
+  const artists = await getArtist(data.artists.map(e => e.id))
+  
+  return { ...data, artists };
 }
 
 /* async function refreshToken(refresh_token: string): Promise<string> {
