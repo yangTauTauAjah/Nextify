5d19da212427a3f98d99e14d7f36b448e8b1bc3b
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-28 14:39:10 +0700

    wrapper

--- components/Layout/MainView/CollectionDisplay/CollectionThumbnail.tsx
+++ components/Layout/MainView/CollectionDisplay/CollectionThumbnail.tsx
@@ -1,28 +1,28 @@
-import { CollectionType } from "@/components/stateSlice/SpotifyAPI/interfaces";
+import {
+  CollectionType,
+  ComponentTypeInterface
+} from "@/components/stateSlice/SpotifyAPI/interfaces";
 import { RootState } from "@/components/store";
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 
-const PlaylistThumbnail = ({
-  type = "playlist"
-}: {
-  type?: CollectionType;
-}) => {
+const Wrapper = styled(Box)({
+  height: "15rem",
+  width: "100%"
+});
+
+const PlaylistThumbnail = ({ type = "playlist" }: ComponentTypeInterface) => {
   let collection = useSelector((state: RootState) => state.data[type]);
   return (
-    <Box
-      sx={{
-        height: "15rem",
-        width: "100%"
-      }}>
+    <Wrapper>
       <Image
         src={collection?.images[0].url || ""}
         fill
         style={{ objectFit: "contain" }}
         alt="image"
       />
-    </Box>
+    </Wrapper>
   );
 };
 
