802d9b9fb5606aa326d2e22bd2095f0d487ab15e
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-30 09:42:19 +0700

    change color

--- components/Layout/MobileWidget/Navlink.tsx
+++ components/Layout/MobileWidget/Navlink.tsx
@@ -1,34 +1,34 @@
-import { Box } from "@mui/material"
-import Link from "next/link"
+import { Box } from "@mui/material";
+import Link from "next/link";
 
-export default function NavLink(props: { link: Array<{ href: string, icon: React.ReactNode, text: string }> }) {
-
+export default function NavLink(props: {
+  link: Array<{ href: string; icon: React.ReactNode; text: string }>;
+}) {
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '0.7rem',
-        width: '100%'
-      }}
-    >{
-        props.link.map((e, i) => (
-          <Link key={i} href={e.href} legacyBehavior>
-            <a
-              style={{
-                display: 'flex', 
-                flexDirection: 'column', 
-                fontSize: '0.5rem', 
-                gap: '0.3rem', 
-                alignItems: 'center' 
-              }}
-            >
-              {e.icon}
-              <p>{e.text}</p>
-            </a>
-          </Link>
-        ))
-      }</Box>
-  )
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0.7rem",
+        width: "100%"
+      }}>
+      {props.link.map((e, i) => (
+        <Link key={i} href={e.href} legacyBehavior>
+          <a
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              fontSize: "0.8rem",
+              gap: "0.3rem",
+              alignItems: "center",
+              color: "rgba(150, 150, 150, 1)"
+            }}>
+            {e.icon}
+            <p>{e.text}</p>
+          </a>
+        </Link>
+      ))}
+    </Box>
+  );
 }
