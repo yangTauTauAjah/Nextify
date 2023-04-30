a70671c8de3fa25498be1cb311815394f0ee57dc
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-30 09:40:30 +0700

    redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+/* const {randomBytes} = require('crypto')
+const {REDIRECT_URI} = require('./components/stateSlice/SpotifyAPI/index.ts') */
+const access_scope = []
+const REDIRECT_URI = 'http://localhost:3000/callback'
+
+const AUTH_URL = `
+https://accounts.spotify.com/authorize
+?response_type=code
+&client_id=${process.env.CLIENT_ID}
+&scope=${access_scope.join(',')}
+&redirect_uri=${REDIRECT_URI}
+`.replace(/[\s\t\n\r]/g, '')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -10,6 +23,16 @@ const nextConfig = {
         pathname: '/**',
       }
     ],
+  },
+  async redirects() {
+
+    return [
+      {
+        source: '/login',
+        destination: `${AUTH_URL}`,
+        permanent: true,
+      },
+    ]
   },
 }
 
