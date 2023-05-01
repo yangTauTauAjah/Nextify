51e363ae4cabc3095f3b38cbeee81fe2c3cbffba
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-01 18:52:41 +0700

    add error type checking

--- components/stateSlice/SpotifyAPI/index.ts
+++ components/stateSlice/SpotifyAPI/index.ts
@@ -4,6 +4,8 @@ import {
   AlbumObject,
   ArtistObject,
   BearerToken,
+  CategoryObject,
+  ErrorRequestInterface,
   PlaylistObject,
   RefreshToken,
   TrackObject,
@@ -13,14 +15,18 @@ import {
 const BASE_PATH = "https://api.spotify.com/v1";
 export const REDIRECT_URI = "http://localhost:3000/callback";
 
-async function getAccessToken(): Promise<BearerToken>;
-async function getAccessToken(code: string): Promise<RefreshToken>;
+export async function getAccessToken(): Promise<
+  BearerToken | ErrorRequestInterface
+>;
+export async function getAccessToken(
+  code: string
+): Promise<RefreshToken | ErrorRequestInterface>;
 
-async function getAccessToken(
+export async function getAccessToken(
   code?: string
-): Promise<BearerToken | RefreshToken> {
+): Promise<BearerToken | RefreshToken | ErrorRequestInterface> {
   if (code && code !== "") {
-    const data: RefreshToken = await fetch(
+    const data: RefreshToken | ErrorRequestInterface = await fetch(
       "https://accounts.spotify.com/api/token",
       {
         method: "post",
@@ -29,12 +35,13 @@ async function getAccessToken(
             "Basic " +
             Buffer.from(
               process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-            ),
+            ).toString("base64"),
           "Content-Type": "application/x-www-form-urlencoded"
         },
         body: `code=${code}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}`
       }
     ).then((data) => data.json());
+
     return data;
   } else {
     const data: BearerToken = await fetch(
@@ -51,15 +58,19 @@ async function getAccessToken(
   }
 }
 
-async function refreshToken(refresh_token: string): Promise<AccessToken> {
-  const data: AccessToken = await fetch(
+async function refreshToken(
+  refresh_token: string
+): Promise<AccessToken | ErrorRequestInterface> {
+  const data: AccessToken | ErrorRequestInterface = await fetch(
     "https://accounts.spotify.com/api/token",
     {
       method: "post",
       headers: {
         Authorization:
           "Basic " +
-          Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString('base64'),
+          Buffer.from(
+            process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
+          ).toString("base64"),
         "Content-Type": "application/x-www-form-urlencoded"
       },
       body: `grant_type=refresh_token&refresh_token=${refresh_token}&client_secret=${process.env.CLIENT_SECRET}`
@@ -70,6 +81,7 @@ async function refreshToken(refresh_token: string): Promise<AccessToken> {
 }
 
 export async function getUser(id: string): Promise<UserObject> {
+  /* @ts-ignore */
   const { access_token } = await getAccessToken();
   let path = `${BASE_PATH}/users/${id}`;
   const data = await fetch(path, {
@@ -78,54 +90,72 @@ export async function getUser(id: string): Promise<UserObject> {
   return data;
 }
 
-export async function getCurrentUserProfile(code: string): Promise<UserObject> {
-  const { access_token } = await refreshToken(code);
-  let path = `${BASE_PATH}/me`;
-  const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
-  }).then((data) => data.json());
-  return data;
+export async function getCurrentUserProfile(
+  token: string
+): Promise<UserObject | undefined> {
+  const access_token = await refreshToken(token);
+  if ((access_token as AccessToken).access_token) {
+    let path = `${BASE_PATH}/me`;
+    const data = await fetch(path, {
+      headers: {
+        Authorization: `Bearer ${(access_token as AccessToken).access_token}`
+      }
+    }).then((data) => data.json());
+    return data;
+  }
 }
 
-export async function getCurrentUserSavedAlbum(code: string): Promise<AlbumObject[]> {
-  const { access_token } = await refreshToken(code);
-  let path = `${BASE_PATH}/me/albums`;
-  const data: { items: { album: AlbumObject }[] } = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
-  }).then((data) => data.json());
+export async function getCurrentUserSavedAlbum(
+  token: string
+): Promise<AlbumObject[] | void> {
+  const access_token = await refreshToken(token);
+  if ("access_token" in access_token) {
+    let path = `${BASE_PATH}/me/albums`;
+    const data: { items: { album: AlbumObject }[] } = await fetch(path, {
+      headers: { Authorization: `Bearer ${access_token.access_token}` }
+    }).then((data) => data.json());
 
-  return data.items.map((e) => e.album);
+    return data.items.map((e) => e.album);
+  }
 }
 
 export async function getCurrentUserFollowedArtist(
-  code: string
-): Promise<ArtistObject[]> {
-  const { access_token } = await refreshToken(code);
-  let path = `${BASE_PATH}/me/following?type=artist`;
-  const data: { artists: { items: ArtistObject[] } } = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
-  }).then((data) => data.json());
-  
-  return data.artists.items;
+  token: string
+): Promise<ArtistObject[] | void> {
+  const access_token = await refreshToken(token);
+  if ("access_token" in access_token) {
+    let path = `${BASE_PATH}/me/following?type=artist`;
+    const data: { artists: { items: ArtistObject[] } } = await fetch(path, {
+      headers: { Authorization: `Bearer ${access_token.access_token}` }
+    }).then((data) => data.json());
+
+    return data.artists.items;
+  }
 }
 
-export async function getCurrentUserPlaylist(code: string): Promise<PlaylistObject[]> {
-  const { access_token } = await refreshToken(code);
-  let path = `${BASE_PATH}/me/playlists`;
-  const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
-  }).then((data) => data.json());
+export async function getCurrentUserPlaylist(
+  token: string
+): Promise<PlaylistObject[] | void> {
+  const access_token = await refreshToken(token);
+  if ("access_token" in access_token) {
+    let path = `${BASE_PATH}/me/playlists`;
+    const data = await fetch(path, {
+      headers: { Authorization: `Bearer ${access_token.access_token}` }
+    }).then((data) => data.json());
 
-  return data.items;
+    return data.items;
+  }
 }
 
-export async function getArtist(ids: string[]): Promise<ArtistObject[]> {
-  const { access_token } = await getAccessToken();
-  let path = `${BASE_PATH}/artists?ids=${ids.join(",")}`;
-  const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
-  }).then((data) => data.json());
-  return data.artists;
+export async function getArtist(ids: string[]): Promise<ArtistObject[] | void> {
+  const token = await getAccessToken();
+  if ("access_token" in token) {
+    let path = `${BASE_PATH}/artists?ids=${ids.join(",")}`;
+    const data = await fetch(path, {
+      headers: { Authorization: `Bearer ${token.access_token}` }
+    }).then((data) => data.json());
+    return data.artists;
+  }
 }
 
 export async function getArtistAlbum(
@@ -134,8 +164,10 @@ export async function getArtistAlbum(
   include_groups?: string[],
   limit?: number,
   offset?: number
-): Promise<AlbumObject[]> {
-  const { access_token } = await getAccessToken();
+): Promise<AlbumObject[] | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/artists/${id}/albums?`;
   if (market) path += `market=${market}`;
   if (include_groups && include_groups.length > 0)
@@ -143,7 +175,7 @@ export async function getArtistAlbum(
   if (limit) path += `limit=${limit}`;
   if (offset) path += `offset=${offset}`;
   const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
   return data.items;
 }
@@ -151,21 +183,27 @@ export async function getArtistAlbum(
 export async function getArtistTopTrack(
   id: string,
   market: string
-): Promise<TrackObject[]> {
-  const { access_token } = await getAccessToken();
+): Promise<TrackObject[] | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/artists/${id}/top-tracks?market=${market}`;
   const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
 
   return data.tracks;
 }
 
-export async function getRelatedArtist(id: string): Promise<ArtistObject[]> {
-  const { access_token } = await getAccessToken();
+export async function getRelatedArtist(
+  id: string
+): Promise<ArtistObject[] | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/artists/${id}/related-artists`;
   const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
 
   return data.artists;
@@ -174,37 +212,110 @@ export async function getRelatedArtist(id: string): Promise<ArtistObject[]> {
 export async function getPlaylist(
   id: string,
   fields?: string
-): Promise<PlaylistObject> {
-  const { access_token } = await getAccessToken();
+): Promise<PlaylistObject | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/playlists/${id}`;
   if (fields && fields !== "") path += `?fields=${fields}`;
   const data = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
+
   const owner = await getUser(data.owner.id);
+  if (!owner) return;
+
   return { ...data, owner };
 }
 
-export async function getAlbum(id: string): Promise<AlbumObject> {
-  const { access_token } = await getAccessToken();
+export async function getAlbum(id: string): Promise<AlbumObject | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/albums/${id}`;
   const data: AlbumObject = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
 
   const artists = await getArtist(data.artists.map((e) => e.id));
+  if (!artists) return;
 
   return { ...data, artists };
 }
 
-export async function getTrack(id: string): Promise<TrackObject> {
-  const { access_token } = await getAccessToken();
+export async function getTrack(id: string): Promise<TrackObject | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
   let path = `${BASE_PATH}/tracks/${id}`;
   const data: TrackObject = await fetch(path, {
-    headers: { Authorization: `Bearer ${access_token}` }
+    headers: { Authorization: `Bearer ${token.access_token}` }
   }).then((data) => data.json());
 
   const artists = await getArtist(data.artists.map((e) => e.id));
+  if (!artists) return;
 
   return { ...data, artists };
+}
+
+export async function getSingleBrowseCategories(
+  id: string,
+  country?: string,
+  locale?: string,
+): Promise<CategoryObject | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
+  let path = `${BASE_PATH}/browse/categories/${id}?`;
+  if (country) path += `country=${country}`;
+  if (locale) path += `locale=${locale}`;
+
+  const data: CategoryObject = await fetch(path, {
+    headers: { Authorization: `Bearer ${token.access_token}` }
+  }).then((data) => data.json());
+
+  return data;
+}
+
+export async function getSeveralBrowseCategories(
+  country?: string,
+  locale?: string,
+  limit?: string,
+  offset?: string
+): Promise<CategoryObject[] | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
+  let path = `${BASE_PATH}/browse/categories?`;
+  if (country) path += `country=${country}`;
+  if (locale) path += `locale=${locale}`;
+  if (limit) path += `limit=${limit}`;
+  if (offset) path += `offset=${offset}`;
+
+  const data: { categories: { items: CategoryObject[] } } = await fetch(path, {
+    headers: { Authorization: `Bearer ${token.access_token}` }
+  }).then((data) => data.json());
+
+  return data.categories.items;
+}
+
+export async function getCategoryPlaylists(
+  id: string,
+  country?: string,
+  limit?: number,
+  offset?: number
+): Promise<PlaylistObject[] | void> {
+  const token = await getAccessToken();
+  if (!("access_token" in token)) return;
+
+  let path = `${BASE_PATH}/browse/categories/${id}/playlists?`;
+  if (country) path += `country=${country}`;
+  if (limit) path += `limit=${limit}`;
+  if (offset) path += `offset=${offset}`;
+
+  const data: { playlists: { items: PlaylistObject[] } } = await fetch(path, {
+    headers: { Authorization: `Bearer ${token.access_token}` }
+  }).then((data) => data.json());
+
+  return data.playlists.items;
 }
