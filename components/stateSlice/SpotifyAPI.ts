334c31ec22d4d72682ff7284a8cf44230b5b111c
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-03 04:41:06 +0700

    new reducers

--- components/stateSlice/SpotifyAPI.ts
+++ components/stateSlice/SpotifyAPI.ts
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { AlbumObject, PlaylistObject, UserObject } from "../interfaces";
+import { AlbumObject, PlaylistObject, TrackObject, UserObject } from "../interfaces";
 
 export type DisplaySlice = {
+  timestamp: number,
+  activeLink: number,
+  nowPlaying?: TrackObject,
   user?: UserObject,
   album?: AlbumObject,
   playlist?: PlaylistObject,
   track?: PlaylistObject
 }
 
-const initialState: DisplaySlice = {};
+const initialState: DisplaySlice = {timestamp: 0, activeLink: 0};
 
 export const display = createSlice({
   name: "playlist",
   initialState,
   reducers: {
+    setTimestamp(state, action: PayloadAction<number>) {
+      state.activeLink = action.payload
+    },
+    setActiveLink(state, action: PayloadAction<number>) {
+      state.activeLink = action.payload
+    },
+    setNowPlaying(state, action: PayloadAction<TrackObject>) {
+      state.nowPlaying = action.payload
+    },
     setUser(state, action: PayloadAction<UserObject>) {
       state.user = action.payload
     },
@@ -30,6 +42,6 @@ export const display = createSlice({
   }
 });
 
-export const { setPlaylist, setAlbum, setTrack, setUser } = display.actions;
+export const { setPlaylist, setAlbum, setTrack, setUser, setActiveLink, setNowPlaying, setTimestamp } = display.actions;
 
 export default display.reducer;
