18eb0a95e7a337f26e78b54d22be80d82ada5e54
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-02 09:58:26 +0700

    moving files

---
+++ components/stateSlice/SpotifyAPI.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { AlbumObject, PlaylistObject, UserObject } from "../interfaces";
+
+export type DisplaySlice = {
+  user?: UserObject,
+  album?: AlbumObject,
+  playlist?: PlaylistObject,
+  track?: PlaylistObject
+}
+
+const initialState: DisplaySlice = {};
+
+export const display = createSlice({
+  name: "playlist",
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<UserObject>) {
+      state.user = action.payload
+    },
+    setPlaylist(state, action: PayloadAction<PlaylistObject>) {
+      state.playlist = action.payload;
+    },
+    setAlbum(state, action: PayloadAction<AlbumObject>) {
+      state.album = action.payload;
+    },
+    setTrack(state, action: PayloadAction<PlaylistObject>) {
+      state.track = action.payload;
+    }
+  }
+});
+
+export const { setPlaylist, setAlbum, setTrack, setUser } = display.actions;
+
+export default display.reducer;
