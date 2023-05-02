d74404072eeeea080daddeb85baf662141e9d8a7
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-03 04:39:13 +0700

    fix access_scope

--- next.config.js
+++ next.config.js
@@ -1,39 +1,48 @@
-/** @type {import('next').NextConfig} */
-/* const {randomBytes} = require('crypto')
-const {REDIRECT_URI} = require('./components/stateSlice/SpotifyAPI/index.ts') */
-const access_scope = []
-const REDIRECT_URI = 'http://localhost:3000/callback'
+const access_scope = [
+  "playlist-read-private",
+  "user-modify-playback-state",
+  "user-library-read",
+  "user-library-modify",
+  "user-follow-read",
+  "user-read-email user-read-private",
+  "user-read-currently-playing",
+  "user-read-recently-played"
+];
+const REDIRECT_URI = "http://localhost:3000/callback";
 
 const AUTH_URL = `
 https://accounts.spotify.com/authorize
 ?response_type=code
 &client_id=${process.env.CLIENT_ID}
-&scope=${access_scope.join(',')}
+&scope=${encodeURI(access_scope.join(" "))}
 &redirect_uri=${REDIRECT_URI}
-`.replace(/[\s\t\n\r]/g, '')
+`.replace(/[\s\t\n\r]/g, "");
+
+/** @type {import('next').NextConfig} */
+/* const {randomBytes} = require('crypto')
+const {REDIRECT_URI} = require('./components/stateSlice/SpotifyAPI/index.ts') */
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**"
       }
-    ],
-  },
-  async redirects() {
-
-    return [
-      {
-        source: '/login',
-        destination: `${AUTH_URL}`,
-        permanent: true,
-      },
     ]
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: AUTH_URL,
+        permanent: false
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
