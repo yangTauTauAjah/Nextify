1b58eede3633bb8ddb8a3abb4a53271b106ef3e5
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-04 06:17:22 +0700

    accepting props

--- components/Layout/MainView/HomePage/Recent/Greeting.tsx
+++ components/Layout/MainView/HomePage/Recent/Greeting.tsx
@@ -1,10 +1,10 @@
 import { History, NotificationsNone, Settings } from '@mui/icons-material'
 import React from 'react'
 
-function Greeting() {
+function Greeting({message}: {message: string}) {
   return (
     <div style={{ marginBlock: '1rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <h1>Selamat beristirahat</h1>
+      <h1>{message}</h1>
       <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
         <NotificationsNone sx={{ fontSize: '2rem' }} />
         <History sx={{ fontSize: '2rem' }} />
--- components/Layout/MainView/HomePage/Recent/index.tsx
+++ components/Layout/MainView/HomePage/Recent/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Greeting from './Greeting'
-import Header from './Header'
-import SongList from './SongList'
-import { Stack } from '@mui/material'
+import React from "react";
+import Greeting from "./Greeting";
+import Header from "./Header";
+import SongList from "./SongList";
+import { Stack } from "@mui/material";
+import { PlaylistObject } from "@/components/interfaces";
 
-
-
-function Recent() {
+function Recent({
+  message,
+  playlists
+}: {
+  message: string;
+  playlists: PlaylistObject[];
+}) {
   return (
     <Stack gap={2}>
-      <Greeting />
+      <Greeting message={message} />
       <Header />
-      <SongList />
+      <SongList playlists={playlists} />
     </Stack>
-  )
+  );
 }
 
-export default Recent
+export default Recent;
