28113fc82be011281e78e03505d959ec6f56cb6c
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:08:05 +0700

    make hoverable

--- components/Layout/Sidebar/InstallApp.tsx
+++ components/Layout/Sidebar/InstallApp.tsx
@@ -1,32 +1,22 @@
-import { DownloadForOfflineOutlined } from '@mui/icons-material'
-import { Box, useTheme } from '@mui/material'
-import Link from 'next/link'
-import React from 'react'
+import { DownloadForOfflineOutlined } from "@mui/icons-material";
+import { Box, useTheme } from "@mui/material";
+import Link from "next/link";
+import React from "react";
 
 function InstallApp() {
-
-  const Theme = useTheme()
+  const Theme = useTheme();
 
   return (
-    <Link href='/' legacyBehavior>
+    <Link href="/" legacyBehavior>
       <Box
-        component='a'
-        sx={{
-          height: '3rem',
-          flexShrink: 0,
-          display: 'none',
-          gap: '1rem',
-          alignItems: 'center',
-          [Theme.breakpoints.up('sm')]: {
-            display: 'flex'
-          }
-        }}
-      >
+        component="a"
+        className="white-hover cursor-pointer flex items-center gap-1 shrink-0"
+        sx={{ height: "3rem" }}>
         <DownloadForOfflineOutlined fontSize="medium" />
         <div>Install App</div>
       </Box>
     </Link>
-  )
+  );
 }
 
-export default InstallApp
+export default InstallApp;
