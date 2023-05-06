bd89287dd09d86c4ac0d5c024ca4a734281615f4
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:07:21 +0700

    changed href props to id

--- components/Layout/Sidebar/Playlists.tsx
+++ components/Layout/Sidebar/Playlists.tsx
@@ -1,36 +1,43 @@
-import { Box, useTheme } from "@mui/material";
+import { PlaylistObject } from "@/components/interfaces";
+import { Box, Typography, useTheme } from "@mui/material";
 import Link from "next/link";
 
-export default function PlayLists(props: { list: Array<{ href: string, text: string }> }) {
-
-  const Theme = useTheme()
+export default function PlayLists({list, currentPlaylistId}: {
+  currentPlaylistId?: string;
+  list: { id: string; text: string }[];
+}) {
+  const Theme = useTheme();
 
   return (
-    <Box sx={{
-      display: 'none',
-      fontSize: '1rem',
-      overflow: 'auto',
-      marginTop: '1rem',
-      marginBottom: 'auto',
-      '&::-webkit-scrollbar': {
-        width: '10px',
-        background: 'rgba(0,0,0,0)',
-      },
-      '&::-webkit-scrollbar-thumb': {
-        background: 'rgba(255,255,255,.0)',
-      },
-      '&:hover::-webkit-scrollbar-thumb': {
-        background: 'rgba(255,255,255,.3)',
-      },
-      [Theme.breakpoints.up('sm')]: {
-        display: 'unset'
-      }
-    }} >{
-        props.list.map((e, i) => (
-          <Link key={i} href={e.href}>
-            <div style={{ height: '2rem', lineHeight: '1.6' }}>{e.text}</div>
-          </Link>
-        ))
-      }</Box>
-  )
+    <Box
+      sx={{
+        fontSize: "1rem",
+        overflow: "auto",
+        marginTop: "1rem",
+        marginBottom: "auto",
+        "&::-webkit-scrollbar": {
+          width: "10px",
+          background: "rgba(0,0,0,0)"
+        },
+        "&::-webkit-scrollbar-thumb": {
+          background: "rgba(255,255,255,.0)"
+        },
+        "&:hover::-webkit-scrollbar-thumb": {
+          background: "rgba(255,255,255,.3)"
+        }
+      }}>
+      {list.map((e, i) => (
+        <Link key={i} href={`/playlist/${e.id}`}>
+          <Typography
+            className={currentPlaylistId !== e.id ? 'white-hover' : ''}
+            sx={{
+              height: "2rem",
+              lineHeight: "1.6"
+            }}>
+            {e.text}
+          </Typography>
+        </Link>
+      ))}
+    </Box>
+  );
 }
