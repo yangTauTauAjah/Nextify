26607fcde001ce9d6111392ad3b387155fb9ea4d
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:11:36 +0700

    display login button for logged-out-user

--- components/Layout/MainView/HomePage/Recent/Greeting.tsx
+++ components/Layout/MainView/HomePage/Recent/Greeting.tsx
@@ -1,17 +1,51 @@
-import { History, NotificationsNone, Settings } from '@mui/icons-material'
-import React from 'react'
+import { RootState } from "@/components/store";
+import { LoginAlert } from "@/pages/collection";
+import { History, NotificationsNone, Settings } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
 
-function Greeting({message}: {message: string}) {
+function Greeting({ message }: { message: string }) {
+  const state = useSelector((state: RootState) => state.data);
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div style={{ marginBlock: '1rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <div
+      className="flex justify-between items-center my-1"
+      style={{ zIndex: "10" }}>
       <h1>{message}</h1>
-      <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-        <NotificationsNone sx={{ fontSize: '2rem' }} />
-        <History sx={{ fontSize: '2rem' }} />
-        <Settings sx={{ fontSize: '2rem' }} />
-      </div>
+      <LoginAlert open={open} handleClose={handleClose} />
+      {state.user ? (
+        <div className="flex items-center gap-1">
+          <NotificationsNone className="cursor-pointer white-hover" sx={{ fontSize: "2rem" }} />
+          <History className="cursor-pointer white-hover" sx={{ fontSize: "2rem" }} />
+          <Settings className="cursor-pointer white-hover" sx={{ fontSize: "2rem" }} />
+        </div>
+      ) : (
+        <Button
+          onClick={handleClickOpen}
+          color="primary"
+          variant="contained"
+          sx={{
+            borderRadius: "10rem",
+            padding: "0.5rem 2rem",
+            fontSize: "1rem",
+            letterSpacing: "2px"
+          }}>
+          Login
+        </Button>
+      )}
     </div>
-  )
+  );
 }
 
-export default Greeting
+export default Greeting;
