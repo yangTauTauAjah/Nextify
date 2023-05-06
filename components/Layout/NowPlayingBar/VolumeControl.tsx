eadc4236b3e7c5ed1e1c69406f1d40fd2e7e448b
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:09:01 +0700

    move custom slider component to the same file

--- components/Layout/NowPlayingBar/VolumeControl.tsx
+++ components/Layout/NowPlayingBar/VolumeControl.tsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React from "react";
 import {
   QueueMusic,
   DevicesOther,
   VolumeUp,
   VolumeOff
-} from '@mui/icons-material';
-import { Box, Slider, Stack } from '@mui/material';
-import CustomSlider from '@/components/CustomSlider';
+} from "@mui/icons-material";
+import { Box, Slider, Stack } from "@mui/material";
+
+function CustomSlider({ style }: { style?: any }) {
+  return (
+    <Slider
+      sx={{
+        ...style,
+        "& .MuiSlider-thumb": {
+          width: "0.8rem",
+          height: "0.8rem"
+        }
+      }}
+      min={0}
+      max={100}
+      defaultValue={50}
+      size="medium"
+      valueLabelDisplay="auto"
+    />
+  );
+}
 
 function VolumeControl() {
   return (
-    <Stack width='33.33%' alignItems='center' justifyContent='flex-end' direction='row' gap='1rem'>
-      <QueueMusic />
-      <DevicesOther />
-      <Stack width='50%' direction='row' gap='0.5rem' alignItems='center'>
-        <VolumeUp />
-        <CustomSlider style={{width: '100%'}} />
+    <Stack
+      width="33.33%"
+      alignItems="center"
+      justifyContent="flex-end"
+      direction="row"
+      gap="1rem">
+      <QueueMusic className="white-hover" />
+      <DevicesOther className="white-hover" />
+      <Stack width="50%" direction="row" gap="0.5rem" alignItems="center">
+        <VolumeUp className="white-hover" />
+        <CustomSlider style={{ width: "100%" }} />
       </Stack>
-    </Stack >
-  )
+    </Stack>
+  );
 }
 
-export default VolumeControl
+export default VolumeControl;
