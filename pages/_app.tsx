a2573baef96729239309684856ce07843a48f9f4
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:06:09 +0700

    play pause functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,31 @@ import "@/styles/globals.css";
 import { themeSettings } from "@/components/theme";
 import { Box, ThemeProvider, createTheme, useTheme } from "@mui/material";
 import type { AppInitialProps, AppProps } from "next/app";
-import { store } from "@/components/store";
+import { RootState, store } from "@/components/store";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import A, { AppContext } from "next/app";
 import {
+  getCurrentUserPlaylist,
   getCurrentUserProfile,
   getCurrentlyPlayingTrack,
   getRecentlyPlayedTrack
 } from "@/components/request";
-import { TrackObject, UserObject } from "@/components/interfaces";
-import { setNowPlaying, setUser } from "@/components/stateSlice/SpotifyAPI";
+import {
+  PlaylistObject,
+  TrackObject,
+  UserObject
+} from "@/components/interfaces";
+import {
+  playNext,
+  setNowPlaying,
+  setUser
+} from "@/components/stateSlice/SpotifyAPI";
 import MobileWidget from "@/components/Layout/MobileWidget";
-import { timeToSec } from "./callback";
-import { useCallback, useEffect, useState } from "react";
-
-export function parseCookie(str: string): Record<string, string | undefined> {
-  return str
-    .split(";")
-    .map((v) => v.split("="))
-    .reduce((acc: NodeJS.Dict<string>, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-      return acc;
-    }, {});
-}
+import { useEffect, useState } from "react";
+import NowPlayingBar from "@/components/Layout/NowPlayingBar";
+import Sidebar from "@/components/Layout/Sidebar";
+import { parseCookie, timeToSec } from "@/components/functions";
+import { useRouter } from "next/router";
 
 interface GlobalState {
   user?: UserObject;
@@ -46,69 +48,115 @@ function Parent({ global, children }: { global?: GlobalState; children: any }) {
   return (
     <Box
       sx={{
+        height: "auto",
+        gridTemplateAreas: `
+          "top-bar         top-bar         top-bar"
+          "nav-bar         main-view       right-sidebar"
+          "now-playing-bar now-playing-bar now-playing-bar"
+        `,
+        gridTemplateColumns: "auto 1fr",
+        gridTemplateRows: "auto 1fr auto",
+        "& > *:nth-child(1)": {
+          gridArea: "main-view",
+          overflow: "auto"
+        },
         [Theme.breakpoints.up("sm")]: {
-          gridTemplateAreas: `
-           "nav-bar         main-view      "
-           "now-playing-bar now-playing-bar"
-         `,
-          gridTemplateColumns: "auto 1fr",
-          gridTemplateRows: "auto 1fr auto"
+          display: "grid",
+          height: "100vh"
         }
       }}>
       {children}
     </Box>
   );
 }
-// styled("div")(({ theme }) => ({
-//   /* gridTemplateAreas: `
-//     "top-bar         top-bar         top-bar"
-//     "nav-bar         main-view       right-sidebar"
-//     "now-playing-bar now-playing-bar now-playing-bar"
-//   `, */
-//   [theme.breakpoints.up("sm")]: {
-//     gridTemplateAreas: `
-//       "nav-bar         main-view      "
-//       "now-playing-bar now-playing-bar"
-//     `,
-//     gridTemplateColumns: "auto 1fr",
-//     gridTemplateRows: "auto 1fr auto"
-//   }
-// }));
 
-function Widget() {
+function Widget({ savedPlaylist }: { savedPlaylist?: PlaylistObject[] }) {
+  const router = useRouter()
   const [IsPlaying, setIsPlaying] = useState(false);
-  const [Timestamp, setTimestamp] = useState(0);
+  const [Timestamp, setTimestamp] = useState(360);
   const [Id, setId] = useState<NodeJS.Timer>();
 
-  const incrementTimestamp = useCallback(() => {
-    setIsPlaying((prev) => !prev);
-    if (!IsPlaying)
-      setId(setInterval(() => setTimestamp((prev) => prev + 1), 1000));
-    else clearInterval(Id);
+  console.log(router.query)
+
+  const state = useSelector((state: RootState) => state.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (IsPlaying) {
+      setId(
+        setInterval(() => {
+          setTimestamp((prev) => prev + 1);
+        }, 1000)
+      );
+    }
+  }, [IsPlaying]);
+
+  useEffect(() => {
+    if (!IsPlaying) clearInterval(Id);
   }, [IsPlaying, Id]);
+
+  useEffect(() => {
+    if (state.nowPlaying && Timestamp > state.nowPlaying.duration_ms / 1000) {
+      setTimestamp(0);
+      setIsPlaying(false);
+      if (state.playingOrder) {
+        dispatch(playNext());
+        setIsPlaying(true);
+      }
+    }
+  }, [
+    dispatch,
+    state.collection,
+    state.playingOrder,
+    state.nowPlaying,
+    Timestamp,
+    IsPlaying
+  ]);
+
+  useEffect(() => setTimestamp(0), [state.playingOrder]);
 
   return (
     <>
-      <MobileWidget
+      {/* <MobileWidget
         Timestamp={Timestamp}
+        setTimestamp={setTimestamp}
         IsPlaying={IsPlaying}
-        incrementTimestamp={incrementTimestamp}
+        incrementTimestamp={setIsPlaying}
+      /> */}
+      <Sidebar
+        currentPlaylistId={router.query.id?.toString()}
+        list={(savedPlaylist || []).map((e) => ({
+          id: e.id,
+          text: e.name
+        }))}
+      />
+      <NowPlayingBar
+        Timestamp={Timestamp}
+        setTimestamp={setTimestamp}
+        IsPlaying={IsPlaying}
+        incrementTimestamp={setIsPlaying}
       />
     </>
   );
 }
 
 const App = (
-  ctx: AppProps & { user?: UserObject; nowPlaying: TrackObject }
+  ctx: AppProps & {
+    user?: UserObject;
+    nowPlaying: TrackObject;
+    savedPlaylist?: PlaylistObject[];
+  }
 ) => {
-  const { Component, pageProps, user, nowPlaying } = ctx;
+  const { Component, pageProps, user, nowPlaying, savedPlaylist } = ctx;
+
+  pageProps.savedPlaylist = savedPlaylist;
 
   return (
     <ThemeProvider theme={Theme}>
       <Provider store={store}>
         <Parent global={{ user, nowPlaying }}>
-          <Component {...pageProps} />
-          <Widget />
+          <Component {...pageProps} savedPlaylist={savedPlaylist} />
+          <Widget savedPlaylist={savedPlaylist} />
         </Parent>
       </Provider>
     </ThemeProvider>
@@ -120,36 +168,41 @@ App.getInitialProps = async (context: AppContext) => {
     ctx: { req, res, pathname }
   } = context;
 
-  const r: AppInitialProps & { user?: UserObject; nowPlaying?: TrackObject } =
-    await A.getInitialProps(context);
+  const r: AppInitialProps & {
+    user?: UserObject;
+    nowPlaying?: TrackObject;
+    savedPlaylist?: PlaylistObject[];
+  } = await A.getInitialProps(context);
 
-  if (req?.headers.cookie) {
-    const Cookie = parseCookie(req.headers.cookie);
+  if (!req?.headers.cookie) return;
 
-    if (Cookie.refresh_token) {
-      const NowPlaying = await getCurrentlyPlayingTrack(Cookie.refresh_token);
-      if (NowPlaying) r.nowPlaying = NowPlaying;
-      else {
-        const recentlyPlayed = await getRecentlyPlayedTrack(
-          Cookie.refresh_token
-        );
-        if (recentlyPlayed) r.nowPlaying = recentlyPlayed[0];
-      }
+  const Cookie = parseCookie(req.headers.cookie);
 
-      if (pathname !== "/callback") {
-        res?.setHeader("Set-Cookie", [
-          `refresh_token=${Cookie.refresh_token}; Max-Age=${timeToSec(
-            0,
-            0,
-            0,
-            3
-          )}; Secure; HttpOnly; Path=/`
-        ]);
+  if (!Cookie.refresh_token) return;
 
-        r.user = await getCurrentUserProfile(Cookie.refresh_token);
-      }
-    }
+  const NowPlaying = await getCurrentlyPlayingTrack(Cookie.refresh_token);
+  const savedPlaylist = await getCurrentUserPlaylist(Cookie.refresh_token);
+
+  if (savedPlaylist) r.savedPlaylist = savedPlaylist;
+  if (NowPlaying) {
+    r.nowPlaying = NowPlaying;
+  } else {
+    const recentlyPlayed = await getRecentlyPlayedTrack(Cookie.refresh_token);
+    if (recentlyPlayed) r.nowPlaying = recentlyPlayed[0];
   }
+
+  if (pathname === "/callback") return;
+
+  res?.setHeader("Set-Cookie", [
+    `refresh_token=${Cookie.refresh_token}; Max-Age=${timeToSec(
+      0,
+      0,
+      0,
+      3
+    )}; Secure; HttpOnly; Path=/`
+  ]);
+
+  r.user = await getCurrentUserProfile(Cookie.refresh_token);
 
   return { ...r };
 };
