73cebf4ae75ae1c9b14e38a1ca622310f0618d2a
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:08:41 +0700

    pass down artist name

--- components/Layout/NowPlayingBar/Widget.tsx
+++ components/Layout/NowPlayingBar/Widget.tsx
@@ -1,59 +1,66 @@
+import { RootState } from "@/components/store";
 import { FavoriteBorder, PictureInPictureAlt } from "@mui/icons-material";
 import { Box, Stack, useTheme } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 
 export default function Widget() {
   const Theme = useTheme();
+  const state = useSelector((state: RootState) => state.data.nowPlaying);
 
   return (
     <Stack
+      className="flex-row items-center gap-1"
       sx={{
-        flexGrow: "1"
-      }}
-      direction="row"
-      alignItems="center"
-      gap="1rem">
+        width: "33.33%"
+      }}>
       <Box
         sx={{
+          borderRadius: "5px",
+          overflow: "hidden",
           height: "3rem",
           aspectRatio: "1"
         }}>
-        <Image sizes="10vw" src="" alt="music" />
+        <Image
+          fill
+          className="object-contain h-full"
+          sizes="10vw"
+          src={state?.album.images[0]?.url || ""}
+          alt="music"
+        />
       </Box>
-      <Box
-        sx={{
-          marginRight: "auto"
-        }}>
-        <Link href="/" legacyBehavior>
-          <a>Test</a>
-        </Link>
+      <Box className="mr-auto">
+        <Box
+          component={Link}
+          sx={{ "&::before": { background: "white" } }}
+          href={`/track/${state?.id}`}>
+          {state?.name}
+        </Box>
         <div>
-          <Link href="/" legacyBehavior>
-            <a>Test</a>
-          </Link>
-          {", "}
-          <Link href="/" legacyBehavior>
-            <a>Test</a>
-          </Link>
+          {state?.artists.reduce((acc: JSX.Element[], curr, index, array) => {
+            acc.push(
+              <Box
+                className="white-hover"
+                component={Link}
+                sx={{
+                  fontSize: "0.7rem",
+                  "&::before": { background: Theme.typography.subtitle1.color }
+                }}
+                key={2 * index}
+                href={`/artist/${curr.id}`}>
+                {curr.name}
+              </Box>
+            );
+            if (index !== array.length - 1) {
+              acc.push(<span key={2 * (index + 1)}>,&nbsp;</span>);
+            }
+            return acc;
+          }, [])}
         </div>
       </Box>
-      <FavoriteBorder
-        sx={{
-          fontSize: "2rem",
-          [Theme.breakpoints.up("sm")]: {
-            fontSize: "small"
-          }
-        }}
-      />
-      <PictureInPictureAlt
-        sx={{
-          fontSize: "2rem",
-          [Theme.breakpoints.up("sm")]: {
-            fontSize: "small"
-          }
-        }}
-      />
+      <FavoriteBorder className="white-hover" />
+      <PictureInPictureAlt className="white-hover" />
     </Stack>
   );
 }
