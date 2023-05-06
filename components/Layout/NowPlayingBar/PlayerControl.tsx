01e4b60edb221b16b062cc763edda60caa5699c1
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:09:21 +0700

    play pause button

--- components/Layout/NowPlayingBar/PlayerControl.tsx
+++ components/Layout/NowPlayingBar/PlayerControl.tsx
@@ -1,86 +1,108 @@
-import React from 'react'
+import React from "react";
 import {
   Shuffle,
   SkipPrevious,
   SkipNext,
   PlayCircle,
   Repeat,
-} from '@mui/icons-material';
-import { Box, Slider, Stack, useTheme } from '@mui/material';
-import CustomSlider from '@/components/CustomSlider';
+  PauseCircleRounded
+} from "@mui/icons-material";
+import { Box, Slider, Stack, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/components/store";
+import { playNext, playPrevious } from "@/components/stateSlice/SpotifyAPI";
 
-function PlayerControl() {
-
-  const Theme = useTheme()
+function PlayerControl({
+  IsPlaying,
+  Timestamp,
+  setTimestamp,
+  incrementTimestamp
+}: {
+  IsPlaying: boolean;
+  Timestamp: number;
+  setTimestamp: (...params: any[]) => any;
+  incrementTimestamp: (...params: any[]) => any;
+}) {
+  const Theme = useTheme();
+  const state = useSelector((state: RootState) => state.data.nowPlaying);
+  const dispatch = useDispatch();
 
   return (
     <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        [Theme.breakpoints.up('sm')]: {
-          justifyContent: 'space-between',
-          width: '33.33%'
-        }
-      }}
-    >
-      <Stack
-        direction='row'
-        gap='1rem'
-        alignItems='center'
-      >
-        <Shuffle
-          sx={{
-            display: 'none',
-            [Theme.breakpoints.up('sm')]: {
-              display: 'flex'
-            }
-          }}
-        />
+      sx={{ width: "33.33%" }}
+      className="flex flex-col h-full justify-between items-center">
+      <Stack direction="row" gap="1rem" alignItems="center">
+        <Shuffle className="hover:pointer white-hover" />
         <SkipPrevious
-          sx={{
-            display: 'none',
-            [Theme.breakpoints.up('sm')]: {
-              display: 'flex'
-            }
-          }} fontSize='large'
+          onClick={() => dispatch(playPrevious())}
+          className="hover:pointer white-hover"
+          fontSize="large"
         />
-        <PlayCircle fontSize='large' />
+        {IsPlaying ? (
+          <PauseCircleRounded
+            onClick={() => incrementTimestamp(false)}
+            className="hover:pointer"
+            fontSize="large"
+          />
+        ) : (
+          <PlayCircle
+            onClick={() => incrementTimestamp(true)}
+            className="hover:pointer"
+            fontSize="large"
+          />
+        )}
+
         <SkipNext
-          sx={{
-            display: 'none',
-            [Theme.breakpoints.up('sm')]: {
-              display: 'flex'
-            }
-          }} fontSize='large'
+          onClick={() => dispatch(playNext())}
+          className="hover:pointer white-hover"
+          fontSize="large"
         />
-        <Repeat
+        <Repeat className="hover:pointer white-hover" />
+      </Stack>
+      <Stack className="flex-row items-center w-full" gap="0.8rem">
+        <Typography
           sx={{
-            display: 'none',
-            [Theme.breakpoints.up('sm')]: {
-              display: 'flex'
+            fontSize: Theme.typography.subtitle1.fontSize,
+            color: Theme.typography.subtitle1.color
+          }}>
+          {state?.duration_ms &&
+            `${Math.floor(Timestamp / 60)}:${Math.round(Timestamp % 60)
+              .toString()
+              .padStart(2, "0")}`}
+        </Typography>
+        <Slider
+          sx={{
+            flexGrow: 1,
+            width: "3rem",
+            "& .MuiSlider-thumb": {
+              width: "0.8rem",
+              height: "0.8rem"
             }
           }}
+          /* @ts-ignore */
+          onChange={(e) => setTimestamp(e.target?.value || 0)}
+          valueLabelDisplay="off"
+          min={0}
+          max={(state?.duration_ms || 0) / 1000}
+          size="medium"
+          value={Timestamp}
         />
-      </Stack>
-      <Stack
-        sx={{
-          display: 'none',
-          [Theme.breakpoints.up('sm')]: {
-            display: 'flex'
-          }
-        }}
-        direction='row'
-        gap='0.8rem'
-        alignItems='center'
-        width='100%'
-      >
-        <div>0:00</div>
-        <CustomSlider style={{ flexGrow: 1, width: '3rem' }} />
-        <div>59:59</div>
+        <Typography
+          sx={{
+            fontSize: Theme.typography.subtitle1.fontSize,
+            color: Theme.typography.subtitle1.color
+          }}>
+          {(state?.duration_ms &&
+            `${Math.floor(state.duration_ms / 1000 / 60)}:${Math.round(
+              (state.duration_ms / 1000) % 60
+            )
+              .toString()
+              .padStart(2, "0")}`) ||
+            "0:00"}
+        </Typography>
       </Stack>
     </Box>
-  )
+  );
 }
 
-export default PlayerControl
+export default PlayerControl;
