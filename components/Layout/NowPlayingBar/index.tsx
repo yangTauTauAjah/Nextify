5a1b79e698a54fa87f761121607c37b08e194dfe
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-06 16:09:39 +0700

    pass down all necessary props

--- components/Layout/NowPlayingBar/index.tsx
+++ components/Layout/NowPlayingBar/index.tsx
@@ -1,40 +1,46 @@
-import { Box, useTheme } from "@mui/material"
-import PlayerControl from "./PlayerControl"
-import VolumeControl from "./VolumeControl"
-import Widget from "./Widget"
+import { Box, useTheme } from "@mui/material";
+import PlayerControl from "./PlayerControl";
+import VolumeControl from "./VolumeControl";
+import Widget from "./Widget";
 
-
-export default function NowPlayingBar() {
-
-  const Theme = useTheme()
+export default function NowPlayingBar({
+  IsPlaying,
+  Timestamp,
+  setTimestamp,
+  incrementTimestamp
+}: {
+  IsPlaying: boolean;
+  Timestamp: number;
+  setTimestamp: (...params: any[]) => any;
+  incrementTimestamp: (...params: any[]) => any;
+}) {
+  const Theme = useTheme();
 
   return (
-    <Box sx={{
-      position: 'fixed',
-      bottom: '4rem',
-      width: '97vw',
-      display: 'flex',
-      justifyContent: 'space-between',
-      gap: '1rem',
-      padding: '10px',
-      marginInline: 'auto',
-      background: '#0fff67',
-      borderRadius: '0.5rem',
-      [Theme.breakpoints.up('sm')]: {
-        gridArea: 'now-playing-bar',
-        display: 'flex',
-        gap: '3rem',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90px',
-        background: '#181818',
-        borderTop: 'solid 1px #222222',
-        paddingInline: '1rem'
-      }
-    }}>
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        display: "flex",
+        padding: "10px",
+        marginInline: "auto",
+        gridArea: "now-playing-bar",
+        gap: "3rem",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90px",
+        background: "#181818",
+        borderTop: "solid 1px #222222",
+        paddingInline: "1rem"
+      }}>
       <Widget />
-      <PlayerControl />
-      {/* <VolumeControl /> */}
+      <PlayerControl
+        Timestamp={Timestamp}
+        setTimestamp={setTimestamp}
+        IsPlaying={IsPlaying}
+        incrementTimestamp={incrementTimestamp}
+      />
+      <VolumeControl />
     </Box>
-  )
+  );
 }
