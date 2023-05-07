60a6768d6d0647f04f03198886463a7b14e2b2c0
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-07 09:37:55 +0700

    open link in new tab

--- components/Layout/MobileWidget/Navlink.tsx
+++ components/Layout/MobileWidget/Navlink.tsx
@@ -1,9 +1,7 @@
-import { setActiveLink } from "@/components/stateSlice/SpotifyAPI";
 import { RootState } from "@/components/store";
 import { Box } from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function NavLink(props: {
   link: Array<{ href: string; icon: React.ReactNode; text: string }>;
@@ -12,23 +10,21 @@ export default function NavLink(props: {
 
   return (
     <Box
+      className="flex justify-between items-center w-full"
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "0.7rem",
-        width: "100%"
+        padding: "0.7rem"
       }}>
       {props.link.map((e, i) => (
         <Link key={i} href={e.href} legacyBehavior>
-          <a style={{
-              display: "flex",
-              flexDirection: "column",
+          <a
+            className="flex flex-col items-center"
+            style={{
               fontSize: "0.8rem",
               gap: "0.3rem",
-              alignItems: "center",
               color: i === active ? "white" : "rgba(150, 150, 150, 1)"
-            }}>
+            }}
+            target={i === 3 ? "_blank" : undefined}
+            rel={i === 3 ? "noopener noreferrer" : undefined}>
             {e.icon}
             <p>{e.text}</p>
           </a>
