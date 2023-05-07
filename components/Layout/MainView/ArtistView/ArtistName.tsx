c465c5c4348989f6b7e82113b2838cca02928eec
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-07 09:42:26 +0700

    remove unused components

--- components/Layout/MainView/ArtistView/ArtistName.tsx
+++
@@ -1,5 +0,0 @@
-import { RootState } from '@/components/store'
-import React from 'react'
-import { useSelector } from 'react-redux'
-
-export default ArtistName
--- components/Layout/MainView/ArtistView/Banner.tsx
+++
@@ -1,6 +0,0 @@
-import { RootState } from "@/components/store";
-import Image from "next/image";
-import React from "react";
-import { useSelector } from "react-redux";
-
-export default Banner;
