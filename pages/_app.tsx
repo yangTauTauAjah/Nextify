0fddad3fe5abf38f898f377bac5aebf1082bd7ce
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-12 19:24:12 +0700

    create Parent element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,55 +38,55 @@ interface GlobalState {
 
 const Theme = createTheme(themeSettings);
 
-// function Parent({ global, children }: { global?: GlobalState; children: any }) {
-//   console.log('Parent Component')
-//   const dispatch = useDispatch();
+function Parent({ global, children }: { global?: GlobalState; children: any }) {
 
-//   const Theme = useTheme();
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     if (global?.user) dispatch(setUser(global.user));
-//     if (global?.nowPlaying) dispatch(setNowPlaying(global.nowPlaying));
-//   }, [dispatch, global?.nowPlaying, global?.user]);
+  const Theme = useTheme();
 
-//   return (
-//     <Box
-//       sx={{
-//         height: "auto",
-//         gridTemplateAreas: `
-//           "top-bar         top-bar         top-bar"
-//           "nav-bar         main-view       right-sidebar"
-//           "now-playing-bar now-playing-bar now-playing-bar"
-//         `,
-//         gridTemplateColumns: "auto 1fr",
-//         gridTemplateRows: "auto 1fr auto",
-//         "& > *:nth-child(1)": {
-//           gridArea: "main-view",
-//           overflow: "auto",
-//           overflowY: "overlay",
-//           "&::-webkit-scrollbar": {
-//             width: "10px",
-//             height: "10px"
-//           },
-//           "&::-webkit-scrollbar-thumb": {
-//             background: "rgba(0,0,0,0)",
-//             transition: "background-color 1s ease-in-out"
-//           },
-//           "&:hover::-webkit-scrollbar-thumb": {
-//             background: "rgba(30, 30, 30, 1)"
-//           }
-//         },
-//         [Theme.breakpoints.up("sm")]: {
-//           display: "grid",
-//           height: "100vh",
-//           overflowX: 'auto',
-//           minWidth: Theme.breakpoints.values.lg
-//         }
-//       }}>
-//       {children}
-//     </Box>
-//   );
-// }
+  useEffect(() => {
+    if (global?.user) dispatch(setUser(global.user));
+    if (global?.nowPlaying) dispatch(setNowPlaying(global.nowPlaying));
+  }, [dispatch, global?.nowPlaying, global?.user]);
+
+  return (
+    <Box
+      sx={{
+        height: "auto",
+        gridTemplateAreas: `
+          "top-bar         top-bar         top-bar"
+          "nav-bar         main-view       right-sidebar"
+          "now-playing-bar now-playing-bar now-playing-bar"
+        `,
+        gridTemplateColumns: "auto 1fr",
+        gridTemplateRows: "auto 1fr auto",
+        "& > *:nth-child(1)": {
+          gridArea: "main-view",
+          overflow: "auto",
+          overflowY: "overlay",
+          "&::-webkit-scrollbar": {
+            width: "10px",
+            height: "10px"
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "rgba(0,0,0,0)",
+            transition: "background-color 1s ease-in-out"
+          },
+          "&:hover::-webkit-scrollbar-thumb": {
+            background: "rgba(30, 30, 30, 1)"
+          }
+        },
+        [Theme.breakpoints.up("sm")]: {
+          display: "grid",
+          height: "100vh",
+          overflowX: 'auto',
+          minWidth: Theme.breakpoints.values.lg
+        }
+      }}>
+      {children}
+    </Box>
+  );
+}
 
 // function Widget({ savedPlaylist }: { savedPlaylist?: PlaylistObject[] }) {
 
