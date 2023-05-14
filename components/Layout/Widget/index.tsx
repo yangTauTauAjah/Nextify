b16d5248aa9e33faf8357fd83572e52b16af9136
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-14 07:48:21 +0700

    enable desktop widget

--- components/Layout/Widget/index.tsx
+++ components/Layout/Widget/index.tsx
@@ -57,12 +57,12 @@ export default function Widget({ savedPlaylist }: { savedPlaylist?: PlaylistObje
 
   return (
     <>
-      {/* <MobileWidget
+      <MobileWidget
         Timestamp={Timestamp}
         setTimestamp={setTimestamp}
         IsPlaying={IsPlaying}
         incrementTimestamp={setIsPlaying}
-      /> */}
+      />
       {/* <Sidebar
         currentPlaylistId={router.query.id?.toString()}
         list={(savedPlaylist || []).map((e) => ({
