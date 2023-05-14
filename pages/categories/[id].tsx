7ba6fd9b9e9f1af53e4182fb7cc96daaf2a074bb
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-14 09:55:04 +0700

    modify

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,64 +1,60 @@
-// import Item from "@/components/Layout/MainView/HomePage/Section/Collection/Item";
-// import {
-//   getCategoryPlaylists,
-//   getSingleBrowseCategories
-// } from "@/components/request";
-// import { PlaylistObject } from "@/components/interfaces";
+import Item from "@/components/Layout/MainView/HomePage/Section/Collection/Item";
+import {
+  getCategoryPlaylists,
+  getSingleBrowseCategories
+} from "@/components/request";
+import { PlaylistObject } from "@/components/interfaces";
 
-import ComingSoon from "@/components/Layout/ComingSoon";
+import Box from "@mui/material/Box";
 
-// import Box from "@mui/material/Box";
+import { GetServerSideProps } from "next";
+import Backlight from "@/components/Layout/MainView/Backlight";
 
-// import { GetServerSideProps } from "next";
-// import Backlight from "@/components/Layout/MainView/Backlight";
+export const getServerSideProps: GetServerSideProps<{
+  category: string;
+  data: PlaylistObject[];
+}> = async ({ params }) => {
+  if (params && "id" in params && params.id && !(params.id instanceof Array)) {
+    const category = await getSingleBrowseCategories(params.id);
+    const data = await getCategoryPlaylists(params.id);
 
-// export const getServerSideProps: GetServerSideProps<{
-//   category: string;
-//   data: PlaylistObject[];
-// }> = async ({ params }) => {
-//   if (params && "id" in params && params.id && !(params.id instanceof Array)) {
-//     const category = await getSingleBrowseCategories(params.id);
-//     const data = await getCategoryPlaylists(params.id);
+    if (data && category) return { props: { category: category.name, data } };
+  }
 
-//     if (data && category) return { props: { category: category.name, data } };
-//   }
+  return { notFound: true };
+};
 
-//   return { notFound: true };
-// };
-
-// export default function Id(props: {
-//   category: string;
-//   data: PlaylistObject[];
-// }) {
-//   const { category, data } = props;
-//   return (
-//     <div className="p-1 pb-10">
-//       <Backlight />
-//       <h1
-//         style={{ fontSize: "2rem", marginBlock: "2rem", marginInline: "1rem" }}>
-//         {category}
-//       </h1>
-//       <Box
-//         className="grid gap-1 mt-1"
-//         sx={{
-//           gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))",
-//           justifyItems: "center"
-//         }}>
-//         {data.map((e) => {
-//           return (
-//             <Item
-//               key={e.id}
-//               href={`/playlist/${e.id}`}
-//               title={e.name}
-//               image={e.images[0].url}
-//               description={e.description}
-//               type="playlist"
-//             />
-//           );
-//         })}
-//       </Box>
-//     </div>
-//   );
-// }
-
-export default ComingSoon
+export default function Id(props: {
+  category: string;
+  data: PlaylistObject[];
+}) {
+  const { category, data } = props;
+  return (
+    <div className="p-1 pb-10">
+      <Backlight />
+      <h1
+        style={{ fontSize: "2rem", marginBlock: "2rem", marginInline: "1rem" }}>
+        {category}
+      </h1>
+      <Box
+        className="grid gap-1 mt-1"
+        sx={{
+          gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))",
+          justifyItems: "center"
+        }}>
+        {data.map((e) => {
+          return (
+            <Item
+              key={e.id}
+              href={`/playlist/${e.id}`}
+              title={e.name}
+              image={e.images[0].url}
+              description={e.description}
+              type="playlist"
+            />
+          );
+        })}
+      </Box>
+    </div>
+  );
+}
--- pages/track/[id].tsx
+++ pages/track/[id].tsx
@@ -1,127 +1,123 @@
-// import { getArtistTopTrack, getTrack } from "@/components/request";
-// import { PlaylistObject, TrackObject } from "@/components/interfaces";
-// import { GetServerSideProps } from "next";
+import { getArtistTopTrack, getTrack } from "@/components/request";
+import { PlaylistObject, TrackObject } from "@/components/interfaces";
+import { GetServerSideProps } from "next";
 
-import ComingSoon from "@/components/Layout/ComingSoon";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import {useTheme} from "@mui/material";
 
-// import Button from "@mui/material/Button";
-// import Box from "@mui/material/Box";
-// import Stack from "@mui/material/Stack";
-// import {useTheme} from "@mui/material";
+import CollectionThumbnail from "@/components/Layout/MainView/CollectionDisplay/CollectionThumbnail";
+import CollectionMetadata from "@/components/Layout/MainView/CollectionDisplay/CollectionMetadata";
+import Tracks from "@/components/Layout/MainView/CollectionDisplay/CollectionTracks";
+import Backlight from "@/components/Layout/MainView/Backlight";
 
-// import CollectionThumbnail from "@/components/Layout/MainView/CollectionDisplay/CollectionThumbnail";
-// import CollectionMetadata from "@/components/Layout/MainView/CollectionDisplay/CollectionMetadata";
-// import Tracks from "@/components/Layout/MainView/CollectionDisplay/CollectionTracks";
-// import Backlight from "@/components/Layout/MainView/Backlight";
+export interface TrackCollectionInterface {
+  trackData: TrackObject;
+  topTrackByArtist: TrackObject[];
+}
 
-// export interface TrackCollectionInterface {
-//   trackData: TrackObject;
-//   topTrackByArtist: TrackObject[];
-// }
+export const getServerSideProps: GetServerSideProps<PlaylistObject> = async ({
+  params,
+  query
+}) => {
+  if (params?.id && !(params.id instanceof Array)) {
+    const track = await getTrack(params.id);
 
-// export const getServerSideProps: GetServerSideProps<PlaylistObject> = async ({
-//   params,
-//   query
-// }) => {
-//   if (params?.id && !(params.id instanceof Array)) {
-//     const track = await getTrack(params.id);
+    if (!track) return { notFound: true };
 
-//     if (!track) return { notFound: true };
+    const {
+      id,
+      name,
+      artists: [artist],
+      album
+    } = track;
 
-//     const {
-//       id,
-//       name,
-//       artists: [artist],
-//       album
-//     } = track;
+    if (!(query["locale"] instanceof Array)) {
+      const tracks = await getArtistTopTrack(
+        artist.id,
+        query["locale"] || "US"
+      );
 
-//     if (!(query["locale"] instanceof Array)) {
-//       const tracks = await getArtistTopTrack(
-//         artist.id,
-//         query["locale"] || "US"
-//       );
+      if (!tracks) return { notFound: true };
 
-//       if (!tracks) return { notFound: true };
+      const props: PlaylistObject = {
+        type: "playlist",
+        id,
+        name,
+        description: "",
+        owner: {
+          id: artist.id,
+          display_name: artist.name,
+          images: artist.images,
+          type: "user"
+        },
+        images: album.images,
+        tracks: {
+          items: tracks.map((e) => ({
+            track: {
+              id: e.id,
+              name: e.name,
+              album: e.album,
+              artists: e.artists,
+              duration_ms: e.duration_ms,
+              explicit: e.explicit
+            }
+          }))
+        }
+      };
 
-//       const props: PlaylistObject = {
-//         type: "playlist",
-//         id,
-//         name,
-//         description: "",
-//         owner: {
-//           id: artist.id,
-//           display_name: artist.name,
-//           images: artist.images,
-//           type: "user"
-//         },
-//         images: album.images,
-//         tracks: {
-//           items: tracks.map((e) => ({
-//             track: {
-//               id: e.id,
-//               name: e.name,
-//               album: e.album,
-//               artists: e.artists,
-//               duration_ms: e.duration_ms,
-//               explicit: e.explicit
-//             }
-//           }))
-//         }
-//       };
+      return { props };
+    }
+  }
 
-//       return { props };
-//     }
-//   }
+  return { notFound: true };
+};
 
-//   return { notFound: true };
-// };
+export default function Id(data: PlaylistObject) {
+  const Theme = useTheme();
 
-// export default function Id(data: PlaylistObject) {
-//   const Theme = useTheme();
-
-//   return (
-//     <Stack className="pb-10" gap={3} sx={{ padding: "2rem 1rem" }}>
-//       <Backlight />
-//       <Box
-//         sx={{
-//           display: "flex",
-//           flexDirection: "column",
-//           gap: "1.5rem",
-//           [Theme.breakpoints.up("sm")]: {
-//             flexDirection: "row",
-//             alignItems: "end",
-//             gap: "3rem"
-//           }
-//         }}>
-//         <CollectionThumbnail collection={data} />
-//         <CollectionMetadata
-//           type="Song"
-//           name={data.name}
-//           description={data.description}
-//           owners={[
-//             {
-//               ...data.owner,
-//               type: "artist",
-//               name: data.owner.display_name,
-//             }
-//           ]}
-//         />
-//       </Box>
-//       <section>
-//         <div>
-//           <Box
-//             component="p"
-//             sx={{ color: ({ typography }) => typography.subtitle1.color }}>
-//             Popular Tracks by
-//           </Box>
-//         </div>
-//         <h1 style={{ marginBlock: "0.7rem", fontSize: "1.7rem" }}>
-//           {data.owner.display_name}
-//         </h1>
-//         <Tracks collection={data} sx={{ marginTop: "2rem" }} />
-//       </section>
-//     </Stack>
-//   );
-// }
-
-export default ComingSoon
+  return (
+    <Stack className="pb-10" gap={3} sx={{ padding: "2rem 1rem" }}>
+      <Backlight />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1.5rem",
+          [Theme.breakpoints.up("sm")]: {
+            flexDirection: "row",
+            alignItems: "end",
+            gap: "3rem"
+          }
+        }}>
+        <CollectionThumbnail collection={data} />
+        <CollectionMetadata
+          type="Song"
+          name={data.name}
+          description={data.description}
+          owners={[
+            {
+              ...data.owner,
+              type: "artist",
+              name: data.owner.display_name,
+            }
+          ]}
+        />
+      </Box>
+      <section>
+        <div>
+          <Box
+            component="p"
+            sx={{ color: ({ typography }) => typography.subtitle1.color }}>
+            Popular Tracks by
+          </Box>
+        </div>
+        <h1 style={{ marginBlock: "0.7rem", fontSize: "1.7rem" }}>
+          {data.owner.display_name}
+        </h1>
+        <Tracks collection={data} sx={{ marginTop: "2rem" }} />
+      </section>
+    </Stack>
+  );
+}
