df886c022139f0e8b980f055d7d39aece4f4637e
yangTauTauAjah <tesakun6464@gmail.com>
2023-05-14 07:45:15 +0700

    disable all children

--- components/Layout/Widget/index.tsx
+++ components/Layout/Widget/index.tsx
@@ -63,13 +63,13 @@ export default function Widget({ savedPlaylist }: { savedPlaylist?: PlaylistObje
         IsPlaying={IsPlaying}
         incrementTimestamp={setIsPlaying}
       /> */}
-      <Sidebar
+      {/* <Sidebar
         currentPlaylistId={router.query.id?.toString()}
         list={(savedPlaylist || []).map((e) => ({
           id: e.id,
           text: e.name
         }))}
-      />
+      /> */}
       {/* <NowPlayingBar
         Timestamp={Timestamp}
         setTimestamp={setTimestamp}
